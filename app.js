0f8a070621b480ac72cb94d94c26a1a811679ae0
akshitaanand <akshita214@gmail.com>
2022-09-30 17:56:58 -0500

    UI

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 'use strict';
+
+localStorage.setItem('city', 'Dallas');
+
+document.getElementById('today-city').innerHTML = localStorage.getItem('city')
+
+document.getElementById("submit").onclick = onSubmit;
+function onSubmit(event){
+    event.preventDefault()
+    var city = document.getElementById('city').value
+    localStorage.setItem('city', city);
+    document.getElementById('today-city').innerHTML = localStorage.getItem('city')
+    console.log(city)
+}
+
